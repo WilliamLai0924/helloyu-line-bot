fc35ef416f7f8726892e345126fc2c3bcca15cfc
YU <w25225575@gmail.com>
2025-03-08 15:55:00 +0800

    response flex msg

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 from flask import Flask, abort, jsonify, request
 from linebot import LineBotApi, WebhookHandler
-from linebot.models import PostbackEvent, TextSendMessage
+from linebot.models import PostbackEvent, TextSendMessage, MessageEvent, TextMessage
+import message
 
 app = Flask(__name__)
 
@@ -14,6 +15,11 @@ whhandler = WebhookHandler(SECRET)
 @app.route('/api/hello', methods=['GET'])
 def hello():
     return jsonify({'message':'Hi~'})
+
+@whhandler.add(MessageEvent,message=TextMessage)
+def handle_message(event):
+    if "買" in event.message.text:
+        line_bot_api.reply_message(event.reply_token, message.create_product_bubble_msg())
 
 @whhandler.add(PostbackEvent)
 def handle_postback(event):
